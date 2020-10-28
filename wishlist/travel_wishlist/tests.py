dc3f0086ba8d0c9d1b7a65d52ab49e54aba8139c
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-28 00:40:49 -0500

    tokyo test

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -14,6 +14,7 @@ class TestHomePage(TestCase):
 
 
 class TestWishList(TestCase):
+
     fixtures = ['test_places']
 
     def test_wishlist_contains_not_visited_places(self):
@@ -23,3 +24,40 @@ class TestWishList(TestCase):
         self.assertContains(response, 'New York')
         self.assertNotContains(response, 'San Francisco')
         self.assertNotContains(response, 'Moab')
+
+
+class TestVisitedPage(TestCase):
+
+    def test_visited_page_shows_empty_list_message_for_empty_database(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'You have not visited any places yet')
+
+
+class TestVisitedList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'San Francisco')
+        self.assertContains(response, 'Moab')
+
+
+class TestAddNewPlace(TestCase):
+
+    def test_add_new_unvisited_place(self):
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'Tokyo', 'visited': False}
+
+        response = self.client.post(add_place_url, new_place_data, follow=True)
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        response_places = response.context['places']
+        self.assertEqual(1, len(response_places))
+        tokyo_from_response = response_places[0]
+
+        tokyo_from_database = Place.objects.get(name="Tokyo", visited=False)
+        self.assertEqual(tokyo_from_database, tokyo_from_response)
