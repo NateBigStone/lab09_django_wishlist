763206aa5e8ef2be99013a8ff3c22f65dd5404b1
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-27 21:50:53 -0500

    templates

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
+from .forms import NewPlaceForm
 
 
 def place_list(r):
-    places = Place.objects.all()
-    return render(r, 'travel_wishlist/wishlist.html', {'places': places})
+
+    if r.method == 'POST':
+        form = NewPlaceForm(r.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(r, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+
+def places_visited(r):
+    visited = Place.objects.filter(visited=True)
+    return render(r, 'travel_wishlist/visited.html', {'visited': visited})
+
+
+def about(r):
+    author = 'Nate'
+    about = 'A website to create a list of places to visit'
+    return render(r, 'travel_wishlist/about.html', {'author': author, 'about': about})
