66c829ae739a3739e4b8bff382f9a304439fe64b
NateBigStone <n.gaut@bigstonemgmt.com>
2020-11-03 23:11:45 -0600

    added views and links

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def place_list(r):
     if r.method == 'POST':
         form = NewPlaceForm(r.POST)
-        place = form.save()
+        place = form.save(commit=False)
+        place.user = r.user
         if form.is_valid():
             place.save()
             return redirect('place_list')
 
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=r.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(r, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
 
+@login_required()
 def places_visited(r):
     visited = Place.objects.filter(visited=True)
     return render(r, 'travel_wishlist/visited.html', {'visited': visited})
 
 
+@login_required()
 def place_was_visited(r, place_pk):
     if r.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
@@ -29,7 +34,14 @@ def place_was_visited(r, place_pk):
     return redirect('place_list')
 
 
+@login_required()
 def about(r):
     author = 'Nate'
     about = 'A website to create a list of places to visit'
     return render(r, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
+
+@login_required()
+def place_details(r, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    return render(r, 'travel_wishlist/place_detail.html', {'place': place})
